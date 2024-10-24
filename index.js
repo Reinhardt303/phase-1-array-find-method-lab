41da650d0e2641d4aca491f597426ab6d6c466c2
Thomas Rains <thomasrains01@gmail.com>
2024-10-23 17:13:46 -0700

    submits array find method

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+ function superbowlWin(wins){
+    for (const record of wins){
+        if(record.result === "W"){
+            return record.year
+        }
+    }
+    
+ }
